10ae6031d9bd17a87f58c36b2571462c9fd4035f
Bolin Wu <Bolin.Wu@ki.se>
2022-05-19 12:28:15 +0200

    enable to create rmd file with one line of code

---
+++ R/UseMyTemplate.R
@@ -0,0 +1,33 @@
+#' @title UseMyTemplate
+#' @description Use the MyTemplate Rmd for starting analysis
+#' @param name name of analysis file
+#' @param open should the file be opened after being created
+#' @param ... arguments to be passed to \link[usethis]{use_template}
+#' @importFrom usethis use_template
+#' @import here
+#' @examples
+#' \donttest{
+#' if(interactive()){
+#'   UseMyTemplate(name = "UseMyTemplate.Rmd")
+#' }
+#' }
+#'
+#' @export
+UseMyTemplate <-
+  function(name = NULL,
+           open = interactive(),
+           ...) {
+
+    if(is.null(name)){
+      name <- "analysis.Rmd"
+    }
+
+    dir.create(file.path(here::here(), "analysis"), showWarnings = FALSE)
+
+    usethis::use_package("usethis")
+
+    usethis::use_template("skeleton.Rmd",
+                 save_as=  paste0("analysis/", name), #"analysis/analysis.Rmd",
+                 data = list(),
+                 package = "BolinRMDTemplate", ..., open = open)
+  }
